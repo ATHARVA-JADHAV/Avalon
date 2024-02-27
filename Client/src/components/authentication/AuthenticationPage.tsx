416fb44f40403237ba030c44b859140dc01124f1
harshdeshmukh21 <deshmukhharsh2122@ternaengg.ac.in>
2024-02-27 13:01:41 +0530

    firebase - 1

--- Client/src/components/authentication/AuthenticationPage.tsx
+++ Client/src/components/authentication/AuthenticationPage.tsx
@@ -1,42 +1,63 @@
-import { CardTitle, CardDescription, CardHeader, CardContent, Card } from "@/components/ui/card"
-import { Label } from "@/components/ui/label"
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
+import {
+  CardTitle,
+  CardDescription,
+  CardHeader,
+  CardContent,
+  Card,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import React from "react";
+import { useState } from "react";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { app } from ""
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+// import { UserAuth } from "../context/AuthContext";
 
 const AuthenticationPage = () => {
   return (
-    <div className="flex flex-row h-screen" > 
-        <div className="left-page w-2/4 h-screen flex items-center justify-center font-bold">
-            <a href="" className="text-white mt-8 ml-8   ">Carbaonara</a>
+    <div className="flex flex-row h-screen">
+      <div className="left-page w-2/4 h-screen flex items-center justify-center font-bold">
+        <a href="" className="text-white mt-8 ml-8   ">
+          Carbaonara
+        </a>
+      </div>
+      <div className="right-page w-full flex items-center justify-center">
+        <div className="form">
+          <Card className="mx-auto max-w-sm">
+            <CardHeader className="space-y-1">
+              <CardTitle className="text-2xl font-bold">Login</CardTitle>
+              <CardDescription>
+                Enter your email and password to login to your account
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <div className="space-y-4">
+                <div className="space-y-2">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    id="email"
+                    placeholder="m@example.com"
+                    required
+                    type="email"
+                  />
+                </div>
+                <div className="space-y-2">
+                  <Label htmlFor="password">Password</Label>
+                  <Input id="password" required type="password" />
+                </div>
+                <Button className="w-full" type="submit">
+                  Sign Up
+                </Button>
+                <Button className=" bg-blue-300"> Sign In with Google</Button>
+              </div>
+            </CardContent>
+          </Card>
         </div>
-        <div className="right-page w-full flex items-center justify-center"><div className="form">
-        <Card className="mx-auto max-w-sm">
-      <CardHeader className="space-y-1">
-        <CardTitle className="text-2xl font-bold">Login</CardTitle>
-        <CardDescription>Enter your email and password to login to your account</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <div className="space-y-4">
-          <div className="space-y-2">
-            <Label htmlFor="email">Email</Label>
-            <Input id="email" placeholder="m@example.com" required type="email" />
-          </div>
-          <div className="space-y-2">
-            <Label htmlFor="password">Password</Label>
-            <Input id="password" required type="password" />
-          </div>
-          <Button className="w-full" type="submit">
-            Sign Up
-          </Button>
-          <Button className=" bg-blue-300"> Sign In with Google</Button>
-    
-        </div>
-      </CardContent>
-    </Card>
-        </div></div>
-        
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AuthenticationPage
+export default AuthenticationPage;
---
+++ Client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC26HuDaKaCAP5ZiOOzRSyurnLZz9jF8Q0",
+  authDomain: "carbonara-67eae.firebaseapp.com",
+  projectId: "carbonara-67eae",
+  storageBucket: "carbonara-67eae.appspot.com",
+  messagingSenderId: "216121501388",
+  appId: "1:216121501388:web:af2b4dfab32b5588554469",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
