d4c5c5794270233397670015c692ab2506a00135
ATHARVA-JADHAV <atharvajadhav550@gmail.com>
2024-07-22 18:43:28 +0530

    new routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,25 @@
-// import Router from "./Router";
-import { BrowserRouter } from "react-router-dom";
-import { AuthContextProvider } from "./context/AuthContext";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./components/Homepage/Homepage";
+import Dashboard from "./components/dashboard/Dashboard";
+import Energyinfo from "./components/Energyconservation/Energyinfo";
+import AuthenticationPage from "./components/authentication/AuthenticationPage";
+import ProductsImpact from "./components/ProductsImpact/ProductsImpact";
+import Blog from "./components/Blogs/Blogpage";
 import Activities from "./components/activity/Activities";
 // import Sidebar from "./components/overlay/Sidebar";
 
 export default function App() {
   return (
-    <>
-      <AuthContextProvider>
-        <BrowserRouter>
-          {/* <Router /> */}
-          {/* <Sidebar */}
-          <Activities />
-        </BrowserRouter>
-      </AuthContextProvider>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/energy" element={<Energyinfo />} />
+        <Route path="/auth" element={<AuthenticationPage />} />
+        <Route path="/products" element={<ProductsImpact />} />
+        <Route path="/activities" element={<Activities />} />
+        <Route path="/blogs" element={<Blog />} />
+      </Routes>
+    </Router>
   );
 }
