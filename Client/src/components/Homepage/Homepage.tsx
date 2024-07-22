c2da8e76717d54b7fefe1f41a40219a5990409c8
ATHARVA-JADHAV <atharvajadhav550@gmail.com>
2024-07-22 19:11:55 +0530

    Update Homepage.tsx

--- Client/src/components/Homepage/Homepage.tsx
+++ Client/src/components/Homepage/Homepage.tsx
@@ -10,19 +10,22 @@ const Homepage = () => {
   return (
     <div className="flex  overflow-hidden flex-col background items-center justify-center h-screen   bg-cover bg-center">
       <div className="two">
-        <div className="logo" id="compss flex flex-col items-center justify-center">
-
-        </div>{" "}
+        <div
+          className="logo"
+          id="compss flex flex-col items-center justify-center"
+        ></div>{" "}
         <div className="flex flex-col items-center justify-center">
-        <p className="text-8xl font-serif font-bold m-8">Carbonara</p>
-        <p className="para text-black">Make the world a better place.</p>  
-        
-        <Button className="w-fit m-4" onClick={() => navigate(`/auth` ) }>Get Started</Button>  
-      </div>
+          <p className="mt-10 text-7xl font-serif font-bold m-8">Carbonara</p>
+          <p className=" my-1 text-2xl para text-black">
+            Make the world a better place.
+          </p>
+
+          <Button className="w-fit m-4" onClick={() => navigate(`/auth`)}>
+            Get Started
+          </Button>
         </div>
-        
-      
-      <div className="eart" id="compss">
+      </div>
+      <div className="earth" id="compss">
         <img src={earth}></img>
       </div>{" "}
     </div>
